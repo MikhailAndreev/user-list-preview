eed981addf4f24b104da75925ad40d21a90d4a15
Mikhail Andreev <mikhail@mircod.com>
2020-08-22 18:42:20 +0300

    little fix app.jsx

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, HashRouter } from "react-router-dom";
 
 import UsersList from "./UsersList/UsersList";
 
